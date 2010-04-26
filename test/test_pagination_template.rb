916ced0090f236f5d3aeb3214e076a29d2d269d2
Cyril David <cyx@sinefunc.com>
2010-04-27 03:14:10 +0800

    changed markup, updated to use new better one

--- test/test_pagination_template.rb
+++ test/test_pagination_template.rb
@@ -9,16 +9,17 @@ class TestPaginationTemplate < Test::Unit::TestCase
     @dataset = ['one', 'two', 'three', 'four', 'five']
     @items = Pagination.paginate(@dataset, :per_page => 1, :page => 1)
   end
-  
+
+  def doc
+    Nokogiri(@render.call)
+  end
+ 
   describe "haml rendering option" do
     setup do
       Object.const_set(:Haml, HamlHere) unless defined?(Haml)
       @render = lambda { Pagination::Template.new(@items).render }
     end
     
-    def doc
-      Nokogiri(@render.call)
-    end
 
     should "be able to render using haml" do
       assert_nothing_raised do
@@ -26,50 +27,79 @@ class TestPaginationTemplate < Test::Unit::TestCase
       end
     end
 
-    should "have a ul > li.prev-link.disabled" do
-      assert_equal 1, 
-        doc.search('ul.pagination > li.prev-link.disabled > a').length
+    should "have no .pagination > a.prev-link" do
+      assert_equal 0, doc.search('.pagination > a.prev-link').length
     end
 
     should "have a ul > li.next-link with page=2" do
       assert_equal 1, 
-        doc.search('ul.pagination > li.next-link > a[href="?page=2"]').length
+        doc.search('div.pagination > a.next-link[href="?page=2"]').length
     end
 
-    should "display the first page as the curretn page" do
-      assert_equal 1, 
-        doc.search('ul.pagination > li > ul.page-numbers > li > span').length
+    should "display the first page as the current page" do
+      assert_equal 1, doc.search('div.pagination > ul > li > span').length
 
-      assert_equal '1', 
-        doc.search('ul.pagination > li > ul.page-numbers > li > span').text
+      assert_equal '1', doc.search('div.pagination > ul > li > span').text
     end
 
     should "display pages 2 to 5 as links" do
       (2..5).each do |page|
         assert_equal 1, 
-          doc.search(%(ul.pagination > li > 
-                       ul.page-numbers > li > a[href="?page=#{page}"])).length
+          doc.search(%(div.pagination > ul > li > 
+                       a[href="?page=#{page}"])).length
 
         assert_equal page.to_s, 
-          doc.search(%(ul.pagination > li > 
-                       ul.page-numbers > li > a[href="?page=#{page}"])).text
+          doc.search(%(div.pagination > ul > li > 
+                       a[href="?page=#{page}"])).text
       end
     end
   end
 
   describe "erb rendering option" do
     setup do
-      Object.send :remove_const, :Haml
+      if defined?(Haml)
+        Object.send :remove_const, :Haml
+      end
+
+      @template = Pagination::Template.new(@items)
+      @render = lambda { @template.render }
     end
   
     should "render without any errors" do
-      template = Pagination::Template.new(@items)
-      assert_kind_of ERB, template.send(:engine)
+      assert_kind_of ERB, @template.send(:engine)
 
       assert_nothing_raised do
-        template.render
+        @render.call
       end
     end
+
+    should "have no .pagination > a.prev-link" do
+      assert_equal 0, doc.search('.pagination > a.prev-link').length
+    end
+
+    should "have a ul > li.next-link with page=2" do
+      assert_equal 1, 
+        doc.search('div.pagination > a.next-link[href="?page=2"]').length
+    end
+
+    should "display the first page as the current page" do
+      assert_equal 1, doc.search('div.pagination > ul > li > span').length
+
+      assert_equal '1', doc.search('div.pagination > ul > li > span').text
+    end
+
+    should "display pages 2 to 5 as links" do
+      (2..5).each do |page|
+        assert_equal 1, 
+          doc.search(%(div.pagination > ul > li > 
+                       a[href="?page=#{page}"])).length
+
+        assert_equal page.to_s, 
+          doc.search(%(div.pagination > ul > li > 
+                       a[href="?page=#{page}"])).text
+      end
+    end
+
   end
 end
 
